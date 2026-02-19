d4ee5666ae5a30bae30ea9d769f49d1bb011accd
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-02-19 03:16:03 +0300

    cpp-poco googletest

--- cpp-poco/README.md
+++ cpp-poco/README.md
@@ -2,9 +2,23 @@
 
 ## Requirements
 
-Build `../c-exprtk-adapter` first
+Build `../c-exprtk-adapter`, `../test/build-googletest.sh` first
 
 ## Build
+
+### Debug with tests
+
+```shell
+rm -rf build && mkdir build && cd build && cmake -DENABLE_TESTS=ON -DCMAKE_BUILD_TYPE=Debug .. && make && ctest --test-dir ./tests/ && cd ..
+```
+
+or:
+
+```shell
+rm -rf build && mkdir build && cmake -DENABLE_TESTS=ON -S . -B build && cmake --build build && cd build && ctest --test-dir ./tests/ && cd ..
+```
+
+### Release
 
 ```shell
 rm -rf build && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make && cd ..
---
+++ cpp-poco/tests/CalcTest.cpp
@@ -0,0 +1,72 @@
+#include "../src/CalcConstants.hpp"
+#include "../src/CalcRequestHandlerFactory.cpp"
+#include "Poco/Net/HTTPClientSession.h"
+#include "Poco/Net/HTTPRequest.h"
+#include "Poco/Net/HTTPResponse.h"
+#include "Poco/Net/HTTPServer.h"
+#include "Poco/Net/HTTPServerParams.h"
+#include "Poco/Net/ServerSocket.h"
+#include <gtest/gtest.h>
+#include <string>
+
+class CalcTest : public ::testing::Test {
+
+protected:
+  const std::string EXPRESSION =
+      "(-abs(pi*2-e-(32-4)/(23+4/5)-(2-4)*(4+6-98.2)+4))+1.9e2";
+  const std::string EXPRESSION_RESULT = "19.9884328904852281993953511118888854980469";
+
+  const std::string NOT_AN_EXPRESSION = "nan";
+
+  Poco::Net::HTTPServer *_server;
+
+  void SetUp() override {
+    Poco::Net::ServerSocket svs(0);
+    Poco::Net::HTTPServerParams *pParams = new Poco::Net::HTTPServerParams;
+    pParams->setKeepAlive(false);
+    _server = new Poco::Net::HTTPServer(new calc::CalcRequestHandlerFactory,
+                                        svs, pParams);
+    _server->start();
+  }
+  void TearDown() override {
+    _server->stop();
+    delete _server;
+  }
+  void performRequest(Poco::Net::HTTPRequest *request, std::string requestBody,
+                      std::string expected) {
+    Poco::Net::HTTPClientSession clientSession(
+        "127.0.0.1", _server->socket().address().port());
+    request->setContentType(calc::TEXT_PLAIN);
+    if (requestBody.empty()) {
+      clientSession.sendRequest(*request);
+    } else {
+      request->setContentLength((int)requestBody.length());
+      clientSession.sendRequest(*request) << requestBody;
+    }
+    Poco::Net::HTTPResponse response;
+    std::string responseBody;
+    std::istream &inputStream = clientSession.receiveResponse(response);
+    Poco::StreamCopier::copyToString(inputStream, responseBody);
+    EXPECT_EQ(response.getStatus(), Poco::Net::HTTPResponse::HTTP_OK);
+    EXPECT_EQ(response.getContentType(), calc::TEXT_PLAIN);
+    EXPECT_EQ(responseBody, expected);
+  }
+};
+
+TEST_F(CalcTest, testWelcome) {
+  Poco::Net::HTTPRequest *request = new Poco::Net::HTTPRequest("GET", "/");
+  performRequest(request, "", calc::WELCOME);
+  delete request;
+}
+
+TEST_F(CalcTest, testExpression) {
+  Poco::Net::HTTPRequest *request = new Poco::Net::HTTPRequest("POST", "/");
+  performRequest(request, EXPRESSION, EXPRESSION_RESULT);
+  delete request;
+}
+
+TEST_F(CalcTest, testNotAnExpression) {
+  Poco::Net::HTTPRequest *request = new Poco::Net::HTTPRequest("POST", "/");
+  performRequest(request, NOT_AN_EXPRESSION, NOT_AN_EXPRESSION);
+  delete request;
+}
