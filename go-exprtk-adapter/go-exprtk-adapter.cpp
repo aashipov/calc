9fc5a93523ff7d82f604bc075fe5c7d7a7df17a1
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-02-21 00:15:03 +0300

    cleaned up

--- go-exprtk-adapter/go-exprtk-adapter.cpp
+++
@@ -1,8 +0,0 @@
-#include "go-exprtk-adapter.h"
-#include "exprtk-adapter.hpp"
-
-double LIB_evaluate(const char *expression) {
-  std::string expression_string(expression);
-  double result = calc::calculate<double>(expression_string);
-  return result;
-}
