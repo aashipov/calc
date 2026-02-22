2b672b681ca2b7e5a819111a855a6f60550abace
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-02-22 03:18:25 +0300

    rust-ntex

--- rust-ntex/src/handler.rs
+++ rust-ntex/src/handler.rs
@@ -5,30 +5,34 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 use std::ffi::{CString, c_char};
 
-use ntex::web::{HttpResponse, Responder, get, post};
+use ntex::web::{HttpResponse, get, post};
 
 const WELCOME: &'static str = "Welcome to calc service\nHTTP POST your expression /";
 
+fn text_response(body: String) -> HttpResponse {
+    return HttpResponse::Ok().content_type("text/plain").body(body);
+}
+
 #[get("{tails:.*}")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body(WELCOME)
+async fn index() -> HttpResponse {
+    return text_response(WELCOME.to_owned());
+}
+
+#[post("{tails:.*}")]
+async fn via_meval(expr: String) -> HttpResponse {
+    let eval = meval::eval_str(expr);
+    match eval {
+        Ok(result) => text_response((result).to_string()),
+        Err(err) => text_response(err.to_string()),
+    }
 }
 
 #[post("/exprtk")]
-async fn via_exprkt(expr: String) -> impl Responder {
+async fn via_exprkt(expr: String) -> HttpResponse {
     let expr_c_string = CString::new(expr).expect("CString::new failed");
     let expr_c_ptr: *const c_char = expr_c_string.as_ptr();
     unsafe {
         let result = calculate(expr_c_ptr);
-        return HttpResponse::Ok().body((result).to_string());
-    }
-}
-
-#[post("{tails:.*}")]
-async fn via_meval(expr: String) -> impl Responder {
-    let eval = meval::eval_str(expr);
-    match eval {
-        Ok(result) => HttpResponse::Ok().body((result).to_string()),
-        Err(err) => HttpResponse::BadRequest().body(err.to_string()),
+        return text_response((result).to_string());
     }
 }
