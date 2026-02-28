99084db138d43d86ab5612da6b353e7b2270ad3f
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-02-28 14:38:01 +0300

    rust-ntex e2e tests

--- rust-ntex/src/calc.rs
+++ rust-ntex/src/calc.rs
@@ -4,9 +4,8 @@ use std::thread;
 
 const SOCKET: &'static str = "0.0.0.0:8080";
 
-fn config(app_under_configuration: &mut ServiceConfig) {
-    app_under_configuration
-        .service(handler::respond_welcome)
+fn config(app: &mut ServiceConfig) {
+    app.service(handler::respond_welcome)
         .service(handler::respond_via_exprkt)
         .service(handler::respond_via_meval);
 }
@@ -17,9 +16,99 @@ async fn main() -> std::io::Result<()> {
     let server = HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
-            .configure(|app_under_configuration| config(app_under_configuration))
+            .configure(|app| config(app))
     })
     .workers(num_workers)
     .bind(SOCKET)?;
     return server.run().await;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ntex::{
+        http::StatusCode,
+        web::{WebResponse, test},
+    };
+
+    const EXPRESSION: &'static str = "(-abs(pi*2-e-(32-4)/(23+4/5)-(2-4)*(4+6-98.2)+4))+1.9e2";
+    const EXPRESSION_RESULT_MEVAL: &'static str = "19.988432890485228";
+    const NOT_AN_EXPRESSION: &'static str = "NaN";
+
+    async fn check_response(resp: WebResponse, expected_body: String) {
+        assert_eq!(resp.status(), StatusCode::OK);
+        let body_bytes = test::read_body(resp).await;
+        let body_str = std::str::from_utf8(&body_bytes).unwrap();
+        assert_eq!(body_str, expected_body);
+    }
+
+    #[ntex::test]
+    async fn test_welcome() {
+        let mut app = test::init_service(App::new().configure(|cfg| config(cfg))).await;
+
+        let req = test::TestRequest::get().uri("/").to_request();
+        let resp = test::call_service(&mut app, req).await;
+
+        check_response(resp, calc_ntex::WELCOME.to_string()).await;
+    }
+
+    #[ntex::test]
+    async fn test_via_meval_expr() {
+        let mut app = test::init_service(App::new().configure(|cfg| config(cfg))).await;
+
+        let req = test::TestRequest::post()
+            .set_payload(EXPRESSION)
+            .uri("/")
+            .to_request();
+        let resp = test::call_service(&mut app, req).await;
+
+        return check_response(resp, EXPRESSION_RESULT_MEVAL.to_string()).await;
+    }
+
+    #[ntex::test]
+    async fn test_via_meval_not_an_expr() {
+        let mut app = test::init_service(App::new().configure(|cfg| config(cfg))).await;
+
+        let req = test::TestRequest::post()
+            .set_payload(NOT_AN_EXPRESSION)
+            .uri("/")
+            .to_request();
+        let resp = test::call_service(&mut app, req).await;
+
+        return check_response(
+            resp,
+            "Evaluation error: unknown variable `NaN`.".to_string(),
+        )
+        .await;
+    }
+
+    #[ntex::test]
+    async fn test_via_exprtk_expr() {
+        let mut app = test::init_service(App::new().configure(|cfg| config(cfg))).await;
+
+        let req = test::TestRequest::post()
+            .set_payload(EXPRESSION)
+            .uri("/exprtk")
+            .to_request();
+        let resp = test::call_service(&mut app, req).await;
+
+        return check_response(resp, EXPRESSION_RESULT_MEVAL.to_string()).await;
+    }
+
+    #[ntex::test]
+    async fn test_via_exprtk_not_an_expr() {
+        let mut app = test::init_service(App::new().configure(|cfg| config(cfg))).await;
+
+        let req = test::TestRequest::post()
+            .set_payload(NOT_AN_EXPRESSION)
+            .uri("/exprtk")
+            .to_request();
+        let resp = test::call_service(&mut app, req).await;
+
+        return check_response(
+            resp,
+            "Evaluation error: unknown variable `NaN`.".to_string(),
+        )
+        .await;
+    }
 }
