d67803162d5fab7ea076ea4fc056a2a61cad09e1
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-03-20 09:36:39 +0300

    jetty bounded thread pool

--- jetty/src/main/java/org/dummy/calc/App.java
+++ jetty/src/main/java/org/dummy/calc/App.java
@@ -4,21 +4,27 @@ import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.util.thread.ThreadPool;
 
 /**
  * App.
  */
 public class App {
+
     static final int HTTP_PORT = 8080;
 
     static Server jetty() {
-        Server server = new Server();
+        int capacity = Runtime.getRuntime().availableProcessors();
+        ThreadPool pool = new QueuedThreadPool(capacity);
+        Server server = new Server(pool, null, null);
         ServerConnector connector = new ServerConnector(server);
+        connector.setHost("0.0.0.0");
         connector.setPort(HTTP_PORT);
         server.setConnectors(new Connector[]{connector});
         ServletContextHandler ctxHandler = new ServletContextHandler();
         ctxHandler.addServlet(CalcServlet.class, "/");
-        server.setHandler(ctxHandler);
+        server.setDefaultHandler(ctxHandler);
         return server;
     }
 
