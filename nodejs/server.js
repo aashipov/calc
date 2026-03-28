300f7317b4fcd51a71637aa568f538166fa640ed
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-03-28 07:37:41 +0300

    Cursor-free nodejs cluster

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const math = require("mathjs");
+const cluster = require("cluster");
+const numCPUs = Math.max(2, require("os").cpus().length);
 
 const welcome =
   "Welcome to calc service\nHTTP POST your expression / (via mathjs)";
@@ -32,14 +34,23 @@ const handler = (request, response) => {
   }
 };
 
-const server = http.createServer(handler).listen(8080);
+const gracefulShutdown = (mainProcess, sig) => {
+  mainProcess.on(sig, () => {
+    Object.values(cluster.workers).forEach((w) => {
+      w.process.kill(sig);
+    });
+    mainProcess.exit(0);
+  });
+};
 
-process.on("SIGTERM", () => {
-  console.log("SIGTERM signal received.");
-  server.close();
-});
-
-process.on("SIGINT", () => {
-  console.log("SIGINT signal received.");
-  server.close();
-});
+if (cluster.isPrimary) {
+  console.log(`Primary process ${process.pid}`);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  process.on("SIGTERM", () => gracefulShutdown(process, "SIGTERM"));
+  process.on("SIGINT", () => gracefulShutdown(process, "SIGINT"));
+} else {
+  console.log(`Worker process ${process.pid}`);
+  const server = http.createServer(handler).listen(8080);
+}
