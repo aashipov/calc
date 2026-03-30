cd70849840e7085c75fddab48be5733a6a37709b
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-03-30 23:59:25 +0300

    scala3-zio-http: AI-free/less test harness re-use

--- scala3-zio-http/src/main/scala/org/dummy/calc/App.scala
+++ scala3-zio-http/src/main/scala/org/dummy/calc/App.scala
@@ -6,6 +6,8 @@ import zio.http.*
 
 object App extends ZIOAppDefault {
 
+  override def gracefulShutdownTimeout: Duration = Duration.Zero
+
   override def run: ZIO[ZIOAppArgs & Scope, Any, Any] =
     Server.serve(CalcHandler.routes).provide(Server.default)
 }
--- scala3-zio-http/src/test/scala/org/dummy/calc/CalcHandlerTest.scala
+++ scala3-zio-http/src/test/scala/org/dummy/calc/CalcHandlerTest.scala
@@ -1,91 +1,29 @@
 package org.dummy.calc
 
-import org.junit.jupiter.api.Test
-import zio.http.{Client, Request, TestClient, URL}
-import zio.{Unsafe, ZIO}
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import zio.http.Server
+import zio.Unsafe
+import java.net.Inet4Address
+import java.net.InetSocketAddress
+import org.junit.jupiter.api.TestInstance
+import java.util.concurrent.TimeUnit
+import org.junit.jupiter.api.AfterAll
+import zio.ExitCode
 
-class CalcHandlerTest:
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CalcHandlerTest extends AppTestBase:
 
-  private val EXPRESSION: String =
-    "(-abs(pi*2-e-(32-4)/(23+4/5)-(2-4)*(4+6-98.2)+4))+1.9e2"
-  private val EVALEX_EXPECTED: String = "19.9884"
-  private val MXPARSER_EXPECTED: String = "19.98843289048526"
-  private val NOT_AN_EXPRESSION: String = "abc"
-  private val EXPRTK_EXPECTED: String = "19.988432890485228"
+  private val executor: java.util.concurrent.ExecutorService =
+    java.util.concurrent.Executors.newSingleThreadExecutor()
 
-  private def doRequest(req: Request): String =
-    Unsafe.unsafe { implicit u =>
-      zio.Runtime.default.unsafe
-        .run {
-          (for
-            client <- ZIO.service[Client]
-            _ <- TestClient.addRoutes(CalcHandler.routes)
-            response <- client.batched(req)
-            body <- response.body.asString
-          yield body).provide(TestClient.layer)
-        }
-        .getOrElse(e => throw e.squash)
-    }
-
-  @Test
-  def welcomeTest(): Unit = {
-    val req = Request.get(URL.root)
-    val body = doRequest(req)
-    Assertions.assertEquals(CalcHandler.WELCOME, body)
+  @BeforeAll
+  def setUp(): Unit = {
+    executor.execute(() => App.main(Array.empty))
+    TimeUnit.SECONDS.sleep(1L)
   }
 
-  @Test
-  def evalexTest(): Unit = {
-    val req = Request.post(URL.root, zio.http.Body.fromString(EXPRESSION))
-    val body = doRequest(req)
-    Assertions.assertEquals(EVALEX_EXPECTED, body)
-  }
-
-  @Test
-  def evalexNotAnExpressionTest(): Unit = {
-    val req =
-      Request.post(URL.root, zio.http.Body.fromString(NOT_AN_EXPRESSION))
-    val body = doRequest(req)
-    Assertions.assertEquals(CalcHandler.NAN, body)
-  }
-
-  @Test
-  def mxparserTest(): Unit = {
-    val req = Request.post(
-      URL.root / CalcHandler.MXPARSER_PATH,
-      zio.http.Body.fromString(EXPRESSION)
-    )
-    val body = doRequest(req)
-    Assertions.assertEquals(MXPARSER_EXPECTED, body)
-  }
-
-  @Test
-  def mxparserNotAnExpressionTest(): Unit = {
-    val req = Request.post(
-      URL.root / CalcHandler.MXPARSER_PATH,
-      zio.http.Body.fromString(NOT_AN_EXPRESSION)
-    )
-    val body = doRequest(req)
-    Assertions.assertEquals(CalcHandler.NAN, body)
-  }
-
-  @Test
-  def exprtkTest(): Unit = {
-    val req = Request.post(
-      URL.root / CalcHandler.EXPRTK_PATH,
-      zio.http.Body.fromString(EXPRESSION)
-    )
-    val body = doRequest(req)
-    Assertions.assertEquals(EXPRTK_EXPECTED, body)
-  }
-
-  @Test
-  def exprtkNotAnExpressionTest(): Unit = {
-    val req = Request.post(
-      URL.root / CalcHandler.EXPRTK_PATH,
-      zio.http.Body.fromString(NOT_AN_EXPRESSION)
-    )
-    val body = doRequest(req)
-    Assertions.assertEquals(CalcHandler.NAN, body)
+  @AfterAll
+  def tearDown(): Unit = {
+    App.exit(ExitCode.success)
+    executor.shutdownNow()
   }
