c18ac442512efb10ead098470098d15ceb0753da
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-04-10 17:03:34 +0300

    python-fastapi tests & documentation

--- python-fastapi/README.md
+++ python-fastapi/README.md
@@ -1,3 +1,23 @@
-# Python FastAPI flavor #
+# Python FastAPI flavor
+
+## Requirements
+
+Build `../c-exprtk-adapter` first
+
+## Build
+
+```shell
+./build.sh
+```
+
+## Run
+
+```shell
+./run.sh
+```
+
+## Performance
+
+`gunicorn` workers count above 8 won't increase throughput on a consumer grade PC
 
 [Swagger](http://0.0.0.0:8080/openapi-ui)
--- python-fastapi/tests/test_calc.py
+++ python-fastapi/tests/test_calc.py
@@ -1,30 +1,40 @@
+from fastapi.testclient import TestClient
+from httpx import Response
+
 from src.app import NAN, WELCOME
 from tests.conf import test_client
 
-EXPRESSION: str = "(-abs(pi*2-e-(32-4)/(23+4/5)-(2-4)*(4+6-98.2)+4))+1.9e2"
+SIMPLE_EXPRESSION: str = "2+2"
+SIMPLE_EXPRESSION_RESULT: str = "4.0"
+COMPLEX_EXPRESSION: str = "(-abs(pi*2-e-(32-4)/(23+4/5)-(2-4)*(4+6-98.2)+4))+1.9e2"
+COMPLEX_EXPRESSION_RESULT: str = "19.988432890485228"
 
-EXPRESSION_RESULT: str = "19.988432890485228"
 
-NOT_AN_EXPRESSION = "abc"
-
-
-def test_welcome(test_client):
-    response = test_client.get("/")
+def test_welcome(test_client: TestClient):
+    response: Response = test_client.get("/")
     assert 200 == response.status_code
     assert WELCOME == response.text
 
 
-def test_expression(test_client):
-    response = test_client.post(
-        url="/", content=EXPRESSION, headers={"Content-Type": "text/plain"}
+def test_simple_expression(test_client: TestClient):
+    response: Response = test_client.post(
+        url="/", content=SIMPLE_EXPRESSION, headers={"Content-Type": "text/plain"}
     )
     assert 200 == response.status_code
-    assert EXPRESSION_RESULT == response.text
+    assert SIMPLE_EXPRESSION_RESULT == response.text
 
 
-def test_not_an_expression(test_client):
-    response = test_client.post(
-        url="/", content=NOT_AN_EXPRESSION, headers={"Content-Type": "text/plain"}
+def test_complex_expression(test_client: TestClient):
+    response: Response = test_client.post(
+        url="/", content=COMPLEX_EXPRESSION, headers={"Content-Type": "text/plain"}
+    )
+    assert 200 == response.status_code
+    assert COMPLEX_EXPRESSION_RESULT == response.text
+
+
+def test_invalid_expression(test_client: TestClient):
+    response: Response = test_client.post(
+        url="/", content=NAN, headers={"Content-Type": "text/plain"}
     )
     assert 200 == response.status_code
     assert NAN.lower() == response.text.lower()
