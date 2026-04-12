f4e435202705376c1fbb6c037fb9b9c593c6efbf
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-04-12 08:30:07 +0300

    nodejs-nestjs

--- nodejs-nestjs/src/main.ts
+++ nodejs-nestjs/src/main.ts
@@ -1,10 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import bodyParser from 'body-parser';
-import { AppModule } from './app.module';
-import { ClusterService } from './cluster.service';
+import { NestFactory } from "@nestjs/core";
+import bodyParser from "body-parser";
+import { AppModule } from "./app.module";
+import { ClusterService } from "./cluster.service";
+
+const HTTP_PORT: number = 8080;
+
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.use(bodyParser.text());
-    await app.listen(8080, '0.0.0.0');
+  const app = await NestFactory.create(AppModule);
+  app.use(bodyParser.text());
+  await app.listen(HTTP_PORT, "0.0.0.0");
 }
 ClusterService.clusterize(bootstrap);
