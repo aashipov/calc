4e32db9789840b7fd70e5ac040b0c6dab84467c7
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-04-20 18:47:28 +0300

    python-fastapi remote debug facility, profile & viz

--- python-fastapi/README.md
+++ python-fastapi/README.md
@@ -16,8 +16,28 @@ Build `../c-exprtk-adapter` first
 ./run.sh
 ```
 
+## Remote Debug
+
+```shell
+./debug.sh
+```
+
+In Zed hit F4 and pick `Remote Debug`
+
+## Profile & visualize
+
+```shell
+./profile.sh
+```
+
+Apply the load, e.g. `../test/calc-wrk.sh`
+
+Stop the `profile.sh` app, call `snakewizw.sh`, explore results in browser
+
 ## Performance
 
-`gunicorn` workers count above 8 won't increase throughput on a consumer grade PC
+`gunicorn` above worker per CPU core won't increase throughput on a consumer grade PC
+
+## API
 
 [Swagger](http://0.0.0.0:8080/openapi-ui)
---
+++ python-fastapi/profile.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+enter_venv() {
+    if [ ! -d ${_SCRIPT_DIR}/${DOT_VENV} ]
+    then
+        mkdir -p ${_SCRIPT_DIR}/${DOT_VENV}
+        python3 -m venv ${_SCRIPT_DIR}/${DOT_VENV}
+        . ${_SCRIPT_DIR}/${DOT_VENV}/bin/activate
+        pip3 install hatch==1.16.3 build==1.4.0
+    else
+        . ${_SCRIPT_DIR}/${DOT_VENV}/bin/activate
+    fi
+}
+
+profile() {
+    local FLAVOR_NAME=`basename ${_SCRIPT_DIR}`
+    python3 -m cProfile -o ${_SCRIPT_DIR}/${FLAVOR_NAME}.prof src/app.py
+}
+
+closure() {
+    # https://stackoverflow.com/a/1482133
+    local _SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$0")")
+    cd ${_SCRIPT_DIR}
+
+    set -e
+
+    local DOT_VENV=.venv
+
+    enter_venv
+    profile
+}
+
+closure
