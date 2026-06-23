09587c95b6428f4b41695b797b3653700a271b1e
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-06-23 07:32:44 +0300

    c-exprtk-adapter debug/release

--- c-exprtk-adapter/release.sh
+++ c-exprtk-adapter/release.sh
@@ -10,11 +10,21 @@ download_exprtk() {
 
 with_cmake() {
     cd ${_SCRIPT_DIR}
-    rm -rf ${BUILD_DIR_NAME}
-    mkdir ${BUILD_DIR_NAME} && cd ${BUILD_DIR_NAME}
-    cmake .. -DCMAKE_INSTALL_PREFIX=${HOME}/.local/ -DCMAKE_BUILD_TYPE=Release #-DENABLE_CALCULATOR=ON
+    rm -rf ${_BUILD_DIR_NAME}
+    mkdir ${_BUILD_DIR_NAME} && cd ${_BUILD_DIR_NAME}
+    cmake .. -DCMAKE_INSTALL_PREFIX=${HOME}/.local/ -DCMAKE_BUILD_TYPE=${_CMAKE_BUILD_TYPE} -DENABLE_CALCULATOR=${_ENABLE_CALCULATOR}
     make
     make install
+}
+
+test_with_calculator() {
+    local _EXECUTABLE=${_SCRIPT_DIR}/${_BUILD_DIR_NAME}/exprtk-calculator/exprtk-calculator
+    if [ -f ${_EXECUTABLE} ]
+    then
+        ${_EXECUTABLE} "2+2"
+        ${_EXECUTABLE} "(-abs(pi*2-e-(32-4)/(23+4/5)-(2-4)*(4+6-98.2)+4))+1.9e2"
+        ${_EXECUTABLE} "nan"
+    fi
 }
 
 closure() {
@@ -22,10 +32,15 @@ closure() {
     # Consistent across Linux bash, Cygwin terminal and Git Bash
     local _SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$0")")
 
-    local BUILD_DIR_NAME="build"
+    local _CMAKE_BUILD_TYPE=Release
+    #local _CMAKE_BUILD_TYPE=Debug
+    local _ENABLE_CALCULATOR=ON
+    local _ENABLE_CALCULATOR=OFF
+    local _BUILD_DIR_NAME=${_CMAKE_BUILD_TYPE}
 
     download_exprtk
     with_cmake
+    test_with_calculator
 }
 
 closure
