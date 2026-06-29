e9d2cd0261de7d13066df0badfdd1d6252c105ff
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-06-29 16:52:54 +0300

    Revert "rust-tiny-http Gemma 4 suggestion on body length"
    This reverts commit b6973d0212ef86a8860946c011226a2f2272e3a7.

--- rust-tiny-http/src/handler.rs
+++ rust-tiny-http/src/handler.rs
@@ -1,5 +1,4 @@
 pub const WELCOME: &'static str = "Welcome to calc service\nHTTP POST your expression /";
-const MAX_BODY_LENGTH: usize = 10240;
 
 fn body_to_string(request: &mut tiny_http::Request) -> String {
     let mut body = String::new();
@@ -21,17 +20,14 @@ fn str_response(request: tiny_http::Request, response_str: &str) -> Result<(), s
 pub fn handler(server: std::sync::Arc<tiny_http::Server>) {
     for mut request in server.incoming_requests() {
         if let &tiny_http::Method::Post = request.method() {
-            let mut response_text = calc_tiny_http::NAN.to_owned();
-            if let Some(body_length) = request.body_length() {
-                if body_length <= MAX_BODY_LENGTH {
-                    let body = body_to_string(&mut request);
-                    if body != calc_tiny_http::NAN {
-                        if request.url().contains(calc_tiny_http::EXPRTK) {
-                            response_text = calc_tiny_http::via_exprtk(body);
-                        } else {
-                            response_text = calc_tiny_http::via_meval(body);
-                        }
-                    }
+            request.url();
+            let body = body_to_string(&mut request);
+            let mut response_text = body.to_owned();
+            if body != calc_tiny_http::NAN {
+                if request.url().contains(calc_tiny_http::EXPRTK) {
+                    response_text = calc_tiny_http::via_exprtk(body);
+                } else {
+                    response_text = calc_tiny_http::via_meval(body);
                 }
             }
             str_response(request, &response_text).ok();
